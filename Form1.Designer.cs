9c9b8cf826ade9bc96fd6a332b66011a6eff155f
Berkant Gunes <gunesberkant96@gmail.com>
2025-07-06 10:31:49 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Project8_RapidApiCurrency
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblDollar = new System.Windows.Forms.Label();
+            this.lblEur = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUnitPrice = new System.Windows.Forms.TextBox();
+            this.rdbDollar = new System.Windows.Forms.RadioButton();
+            this.rdbEur = new System.Windows.Forms.RadioButton();
+            this.rdbPound = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtTotalAmount = new System.Windows.Forms.TextBox();
+            this.lblTotal = new System.Windows.Forms.Label();
+            this.lblPound = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblDollar
+            // 
+            this.lblDollar.AutoSize = true;
+            this.lblDollar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDollar.Location = new System.Drawing.Point(66, 53);
+            this.lblDollar.Name = "lblDollar";
+            this.lblDollar.Size = new System.Drawing.Size(64, 25);
+            this.lblDollar.TabIndex = 0;
+            this.lblDollar.Text = "label1";
+            // 
+            // lblEur
+            // 
+            this.lblEur.AutoSize = true;
+            this.lblEur.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblEur.Location = new System.Drawing.Point(303, 53);
+            this.lblEur.Name = "lblEur";
+            this.lblEur.Size = new System.Drawing.Size(64, 25);
+            this.lblEur.TabIndex = 2;
+            this.lblEur.Text = "label1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(81, 268);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Unit Price:";
+            // 
+            // txtUnitPrice
+            // 
+            this.txtUnitPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtUnitPrice.Location = new System.Drawing.Point(194, 268);
+            this.txtUnitPrice.Name = "txtUnitPrice";
+            this.txtUnitPrice.Size = new System.Drawing.Size(194, 28);
+            this.txtUnitPrice.TabIndex = 5;
+            // 
+            // rdbDollar
+            // 
+            this.rdbDollar.AutoSize = true;
+            this.rdbDollar.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbDollar.Location = new System.Drawing.Point(94, 208);
+            this.rdbDollar.Name = "rdbDollar";
+            this.rdbDollar.Size = new System.Drawing.Size(118, 26);
+            this.rdbDollar.TabIndex = 6;
+            this.rdbDollar.TabStop = true;
+            this.rdbDollar.Text = "U.S. Dollar";
+            this.rdbDollar.UseVisualStyleBackColor = true;
+            // 
+            // rdbEur
+            // 
+            this.rdbEur.AutoSize = true;
+            this.rdbEur.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbEur.Location = new System.Drawing.Point(246, 208);
+            this.rdbEur.Name = "rdbEur";
+            this.rdbEur.Size = new System.Drawing.Size(69, 26);
+            this.rdbEur.TabIndex = 7;
+            this.rdbEur.TabStop = true;
+            this.rdbEur.Text = "Euro";
+            this.rdbEur.UseVisualStyleBackColor = true;
+            // 
+            // rdbPound
+            // 
+            this.rdbPound.AutoSize = true;
+            this.rdbPound.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.rdbPound.Location = new System.Drawing.Point(368, 208);
+            this.rdbPound.Name = "rdbPound";
+            this.rdbPound.Size = new System.Drawing.Size(83, 26);
+            this.rdbPound.TabIndex = 8;
+            this.rdbPound.TabStop = true;
+            this.rdbPound.Text = "Pound";
+            this.rdbPound.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(208, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 46);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Exchange";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // txtTotalAmount
+            // 
+            this.txtTotalAmount.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTotalAmount.Location = new System.Drawing.Point(194, 302);
+            this.txtTotalAmount.Name = "txtTotalAmount";
+            this.txtTotalAmount.Size = new System.Drawing.Size(194, 28);
+            this.txtTotalAmount.TabIndex = 11;
+            // 
+            // lblTotal
+            // 
+            this.lblTotal.AutoSize = true;
+            this.lblTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotal.Location = new System.Drawing.Point(57, 302);
+            this.lblTotal.Name = "lblTotal";
+            this.lblTotal.Size = new System.Drawing.Size(135, 25);
+            this.lblTotal.TabIndex = 10;
+            this.lblTotal.Text = "Total Amount:";
+            // 
+            // lblPound
+            // 
+            this.lblPound.AutoSize = true;
+            this.lblPound.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblPound.Location = new System.Drawing.Point(538, 53);
+            this.lblPound.Name = "lblPound";
+            this.lblPound.Size = new System.Drawing.Size(64, 25);
+            this.lblPound.TabIndex = 12;
+            this.lblPound.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblPound);
+            this.Controls.Add(this.txtTotalAmount);
+            this.Controls.Add(this.lblTotal);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.rdbPound);
+            this.Controls.Add(this.rdbEur);
+            this.Controls.Add(this.rdbDollar);
+            this.Controls.Add(this.txtUnitPrice);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblEur);
+            this.Controls.Add(this.lblDollar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDollar;
+        private System.Windows.Forms.Label lblEur;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUnitPrice;
+        private System.Windows.Forms.RadioButton rdbDollar;
+        private System.Windows.Forms.RadioButton rdbEur;
+        private System.Windows.Forms.RadioButton rdbPound;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtTotalAmount;
+        private System.Windows.Forms.Label lblTotal;
+        private System.Windows.Forms.Label lblPound;
+    }
+}
+
